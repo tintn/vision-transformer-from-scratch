1a6619314b5621b7d69e4b119e02264c1378d228
tin <trung.tin.nguyen0309@gmail.com>
2023-03-06 16:49:20 +0700

    Add train code

---
+++ data.py
@@ -0,0 +1,44 @@
+# Import libraries
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+
+def prepare_data(batch_size=4, num_workers=2, train_sample_size=None, test_sample_size=None):
+    train_transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Resize((32, 32)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomResizedCrop((32, 32), scale=(0.8, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=train_transform)
+    if train_sample_size is not None:
+        # Randomly sample a subset of the training set
+        indices = torch.randperm(len(trainset))[:train_sample_size]
+        trainset = torch.utils.data.Subset(trainset, indices)
+    
+
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=num_workers)
+    
+    test_transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Resize((32, 32)),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                        download=True, transform=test_transform)
+    if test_sample_size is not None:
+        # Randomly sample a subset of the test set
+        indices = torch.randperm(len(testset))[:test_sample_size]
+        testset = torch.utils.data.Subset(testset, indices)
+    
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=num_workers)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    return trainloader, testloader, classes
---
+++ train.py
@@ -0,0 +1,145 @@
+import os
+import torch
+from torch import nn, optim
+
+from utils import save_experiment, save_checkpoint
+from data import prepare_data
+from vit import ViTForClassfication
+
+
+config = {
+    "patch_size": 4,  # Input image size: 32x32 -> 8x8 patches
+    "hidden_size": 48,  # patch_size * patch_size * num_channels
+    "num_hidden_layers": 4,
+    "num_attention_heads": 4,
+    "intermediate_size": 4 * 48, # 4 * hidden_size
+    "hidden_dropout_prob": 0.0,
+    "attention_probs_dropout_prob": 0.0,
+    "initializer_range": 0.02,
+    "image_size": 32,
+    "num_classes": 10, # num_classes of CIFAR10
+    "num_channels": 3,
+    "qkv_bias": True,
+}
+# These are not hard constraints, but are used to prevent misconfigurations
+assert config["hidden_size"] % config["num_attention_heads"] == 0
+assert config['intermediate_size'] == 4 * config['hidden_size']
+assert config['image_size'] % config['patch_size'] == 0
+
+
+class Trainer:
+    """
+    The simple trainer.
+    """
+
+    def __init__(self, model, optimizer, loss_fn, exp_name, device):
+        self.model = model.to(device)
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.exp_name = exp_name
+        self.device = device
+
+    def train(self, trainloader, testloader, epochs, save_model_every_n_epochs=0):
+        """
+        Train the model for the specified number of epochs.
+        """
+        # Keep track of the losses and accuracies
+        train_losses, test_losses, accuracies = [], [], []
+        # Train the model
+        for i in range(epochs):
+            train_loss = self.train_epoch(trainloader)
+            accuracy, test_loss = self.evaluate(testloader)
+            train_losses.append(train_loss)
+            test_losses.append(test_loss)
+            accuracies.append(accuracy)
+            print(f"Epoch: {i+1}, Train loss: {train_loss:.4f}, Test loss: {test_loss:.4f}, Accuracy: {accuracy:.4f}")
+            if save_model_every_n_epochs > 0 and (i+1) % save_model_every_n_epochs == 0 and i+1 != epochs:
+                print('\tSave checkpoint at epoch', i+1)
+                save_checkpoint(self.exp_name, self.model, i+1)
+        # Save the experiment
+        save_experiment(self.exp_name, config, self.model, train_losses, test_losses, accuracies)
+
+    def train_epoch(self, trainloader):
+        """
+        Train the model for one epoch.
+        """
+        self.model.train()
+        total_loss = 0
+        for batch in trainloader:
+            # Move the batch to the device
+            batch = [t.to(self.device) for t in batch]
+            images, labels = batch
+            # Zero the gradients
+            self.optimizer.zero_grad()
+            # Calculate the loss
+            loss = self.loss_fn(self.model(images)[0], labels)
+            # Backpropagate the loss
+            loss.backward()
+            # Update the model's parameters
+            self.optimizer.step()
+            total_loss += loss.item() * len(images)
+        return total_loss / len(trainloader.dataset)
+
+    @torch.no_grad()
+    def evaluate(self, testloader):
+        self.model.eval()
+        total_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for batch in testloader:
+                # Move the batch to the device
+                batch = [t.to(self.device) for t in batch]
+                images, labels = batch
+                
+                # Get predictions
+                logits, _ = self.model(images)
+
+                # Calculate the loss
+                loss = self.loss_fn(logits, labels)
+                total_loss += loss.item() * len(images)
+
+                # Calculate the accuracy
+                predictions = torch.argmax(logits, dim=1)
+                correct += torch.sum(predictions == labels).item()
+        accuracy = correct / len(testloader.dataset)
+        avg_loss = total_loss / len(testloader.dataset)
+        return accuracy, avg_loss
+
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--exp-name", type=str, required=True)
+    parser.add_argument("--batch-size", type=int, default=256)
+    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--lr", type=float, default=1e-2)
+    parser.add_argument("--device", type=str)
+    parser.add_argument("--save-model-every", type=int, default=0)
+
+    args = parser.parse_args()
+    if args.device is None:
+        args.device = "cuda" if torch.cuda.is_available() else "cpu"
+    return args
+
+
+def main():
+    args = parse_args()
+    # Training parameters
+    batch_size = args.batch_size
+    epochs = args.epochs
+    lr = args.lr
+    device = args.device
+    save_model_every_n_epochs = args.save_model_every
+    # Load the CIFAR10 dataset
+    trainloader, testloader, _ = prepare_data(batch_size=batch_size)
+    # Create the model, optimizer, loss function and trainer
+    model = ViTForClassfication(config)
+    optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-2)
+    loss_fn = nn.CrossEntropyLoss()
+    trainer = Trainer(model, optimizer, loss_fn, args.exp_name, device=device)
+    trainer.train(trainloader, testloader, epochs, save_model_every_n_epochs=save_model_every_n_epochs)
+
+
+if __name__ == "__main__":
+    main()
+    
